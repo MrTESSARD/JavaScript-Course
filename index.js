dcc768925c2143ec60028369b575e28e24c57d40
Axel Gromat <axel.gromat@gmail.com>
2021-02-01 23:16:31 +0100

    MD for index and structure

--- index.js
+++
@@ -1,9 +0,0 @@
-// Voici un commentaire in-line en JavaScript. Il est inline car il est possible d'en mettre après une instruction.
-
-
-console.log("Hello from the other side !"); // I must've called a thousand times !!! <- In-line donc juste après.
-
-/* Il existe aussi des commentaires block.
-Il s'agit de commentaires répartis sur plusieurs lignes qui permettent de communiquer plus d'informations.
-Les commentaires ne sont pas lus par l'ordinateur (sauf dans des cas spéciaux et précis)
-mais servent plutôt à communiquer entre humains.*/
