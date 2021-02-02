05d0bd3cd6169cc174c070731ddd731ce6b9d043
Axel Gromat <axel.gromat@gmail.com>
2021-02-02 14:36:07 +0100

    Init

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 "use strict";
 // Numbers and math
+
+let billion = 1000000000; // Notation avec tous les zéros
+
+let altBillion = 1_000_000_000; // Notation avec des underscores pour séparer
+
+let otherBillion = 1e9; // Notation avec des exposants
+
+let cinqMillionsCinq = 5.5e6;//Notations d'un nombre plus précis avec un exposant
+
+let milliseconds = 0.001;// Notation avec des chiffres décimaux
+
+let altMs = 1e-3; //Notation pour les chiffres décimaux avec exposants
+
+let a = 0xff; //255
+let b = 0b11111111; //255
+let c = 0o377;//255
+//console.log(a == b);
+
+let num = 255; // Stringify un chiffre en fonction d'une base de bits
+//console.log(num.toString(16));
+
+//console.log(125.25.toString(16)); // On utilise 2 points pour chaîner un nombre et une méthode
+
