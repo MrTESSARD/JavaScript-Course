bcac1df1385266c4eb29e69d79005cd5b5bec498
Axel Gromat <axel.gromat@gmail.com>
2021-02-02 15:48:17 +0100

    Fin des maths de m****

--- main.js
+++ main.js
@@ -30,8 +30,10 @@ console.log(42 + ' est la réponse'); // dans l'autre sens aussi
 console.log("25"-2);// Mais les autres opérateurs font le calcul
 
 // Récupérer un number depuis une string
-let numString = parseInt('18');
-console.log(numString)
+let numString1 = parseInt('1800.5');
+console.log(numString1);
+let numString2 = parseFloat('1800.525');
+console.log(numString2);
 
 let binary = parseInt('101', 2);//Encodage en binaire
 console.log(binary)
@@ -42,3 +44,43 @@ console.log(numNotString);
 let numIsString = +'11' + +'11'; // Conversion en number avec les +
 console.log(numIsString);
 
+// Math
+
+//Arrondi à l'inférieur
+let floor1 = Math.floor(3.5);//3
+let floor2 = Math.floor(3.9);//3
+let floor3 = Math.floor(-1.1);//-2
+
+//Arrondi à l'excès
+let ceil1 = Math.ceil(3.5);//4
+let ceil2 = Math.ceil(3.1);//4
+let ceil3 = Math.ceil(-1.1);//-1
+
+//Arrondi juste
+let round1 = Math.round(3.5);//4
+let round2 = Math.round(3.1);//3
+let round3 = Math.round(-1.1);//-1
+
+//Troncature [PAS SUPPORTE PAR IE]
+let trunc1 = Math.trunc(3.1)//3
+let trunc2 = Math.trunc(5.2)//5
+let trunc3 = Math.trunc(4.99)//4
+
+let random1 = Math.random();// Un chiffre aléatoire entre 0 et 1 [non inclus]
+console.log(random1);
+
+let max = Math.max(3,5,10,-1,-15);// Valeur la plus grande parmi les chiffres
+
+let min = Math.min(3,5,10,-1,-15);// Valeur la plus petite parmi les chiffres
+
+
+
+
+//Calcul imprécis
+console.log(1e500);//c'est l'infini selon JS
+
+console.log(0.1+0.2);//0.30000000000000004
+
+console.log(0.1.toFixed(20));//0.10000000000000000555
+
+console.log(NaN === NaN);//false
