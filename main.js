3ffec7377d67c59e4e80b50437ee8c603db6551e
Axel Gromat <axel.gromat@gmail.com>
2021-02-02 10:49:48 +0100

    Added infos on JS

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
 // let & const
 
+let maVariable;
 
+console.log(maVariable);
+
+maVariable = 1;
+
+console.log(maVariable);
+
+const uneConst = "Constante";
+
+console.log(uneConst);
+
+// Autres façons de log dans la console
+console.error('Une erreur');
+console.warn('Un avertissement');
+
+const age = 20; // Datatype number
+const temperature = 24.6; // Datatype number
+
+console.log(typeof age);
+
+const name = "Jean-Michel" // Datatype string
+const vitesse = "200" // Datatype string
+
+console.log(typeof vitesse);
+
+const votreNiveauEnJs = null; //Datatype Null, mais va être affiché en tant qu'objet
+
+console.log(typeof votreNiveauEnJs);
+
+let indefini; // Datatype undefined
+
+console.log(typeof indefini);
+
+let unPlusGrandQueZero = true; //Datatype Boolean
+
+console.log(typeof unPlusGrandQueZero);
+
+//Il existe aussi des données de type Symbol qui servent à stocker des infos sensibles et inchangeables.
+
+//BigInt est un type de données qui est proposé pour ES2020.
