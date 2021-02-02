0d487d68e5410674194ffa4dfb0c4f1fe1c4770e
Axel Gromat <axel.gromat@gmail.com>
2021-02-02 11:43:40 +0100

    Strike 1

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 // Concatenate & recent changes
 
+const nom = "Gromat";
+const prenom = "Axel";
+const nomComplet = nom + prenom;
+const age = 26;
+const heureDeTravail = 7;
+const heureDeTravailParSemaine = 35;
+const heureDeTravailCumule = heureDeTravail + heureDeTravailParSemaine;
+
+// 2 exemples de concaténation
+console.log("Je m'appelle " + nom + prenom + " et j'ai " + age + " ans");
+console.log("Je m'appelle " + nomComplet + " et j'ai " + age + " ans");
+console.log(heureDeTravailCumule);
+
+// Recent Changes
+console.log(`Je m'appelle ${nomComplet} et j'ai travaillé ${heureDeTravail} heures aujourd'hui`)
