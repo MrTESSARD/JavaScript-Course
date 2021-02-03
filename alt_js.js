c844f1a8c79944cacb93a906b8c435b3deba6c4f
Axel Gromat <axel.gromat@gmail.com>
2021-02-03 10:43:12 +0100

    math exo 1

--- alt_js.js
+++
@@ -1,9 +0,0 @@
-// EXERCICE 3 : STRINGS
-
-let quote2 = 'I dO nOT lIke gREen eGgS anD HAM';
-
-//  --- VOTRE CODE ICI ---
-
-
-// --- NE PAS TOUCHER EN DESSOUS DE CECI ---
-
--- main.js
+++ main.js
@@ -1,24 +1,10 @@
-// STRING EXERCICE 1
-
-// Add your code here
-
-let debutDeCitation = 'Si debugger, c'est supprimer des bugs, alors ';
-
-
-// Don't edit the code below here!
-
-console.log(citationFinale);
-
-
-// STRING EXERCICE 2
-
-let quote = 'I do not like green eggs and ham. I do not like them, Sam-I-Am.';
-let substring = 'green eggs and ham';
+// MATH EXERCICE 1
 
 // Add your code here
 
 // Don't edit the code below here!
 
-console.log(`La citation possède ${ quoteLength } caractères.`);
+console.log(resultatFinal);
+console.log(pairImpair);
 
-console.log(revisedQuote);
+// MATH EXERCICE 2
