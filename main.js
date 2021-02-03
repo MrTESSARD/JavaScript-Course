91fa118d45a2fb2aedb998f34f366242f13945b6
Axel Gromat <axel.gromat@gmail.com>
2021-02-03 12:53:28 +0100

    fin des exos

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 // Add your code here
 
-let debutDeCitation = 'Si debugger, c'est supprimer des bugs, alors ';
+let debutDeCitation = "Si debugger, c'est supprimer des bugs, alors ";
 
 
 // Don't edit the code below here!
 
-console.log(citationFinale);
+// console.log(citationFinale);
 
 
 // STRING EXERCICE 2
@@ -16,6 +16,7 @@ let quote = 'I do not like green eggs and ham. I do not like them, Sam-I-Am.';
 let substring = 'green eggs and ham';
 
 // Add your code here
+
 
 // Don't edit the code below here!
 
