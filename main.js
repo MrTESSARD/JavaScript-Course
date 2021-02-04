8fe4dbd10766caca79f902f09fab44a6d2e50be3
Axel Gromat <axel.gromat@gmail.com>
2021-02-04 01:44:00 +0100

    Ajout exo 2

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // MATH EXERCICE 1
 
 // Add your code here
+let resultatFinal = 10;
+let pairImpair =1;
 
 // Don't edit the code below here!
 
@@ -8,3 +10,30 @@ console.log(resultatFinal);
 console.log(pairImpair);
 
 // MATH EXERCICE 2
+
+// Enoncé 1 : L'éléphant pèse moins lourd que la souris
+let poidsElephant = 1000;
+let poidsSouris = 2;
+
+// Enoncé 2 : L'autruche est plus grande que le canard
+let tailleAutruche = 2;
+let tailleCanard = 0.3;
+
+// Enoncé 3 : Les deux mots de passes sont équivalents [souvenez vous des valeurs en bits des lettres]
+let mdp1 = 'stromboli';
+let mdp2 = 'stROmBoLi';
+
+// AJOUTER LE CODE ICI
+
+
+
+// NE PAS TOUCHER
+
+
+let testPoids = comparaisonPoids ? "Vrai — Les éléphants pèse moins lourd que des souris !?" : "Faux - Bien sûr qu'un éléphant pèse plus lourd!";
+let testTaille = comparaisonTaille ? "Vrai — Une autruche est en effet plus grande!" : "Faux — Apparemment un canard est plus grand qu'une autruche !?";
+let testMdp = concordanceMdp ? 'Vrai - Les mots de passe concordent' : 'Faux — les mots de passe ne concordent pas - vérifiez votre saisie';
+
+console.log(testPoids);
+console.log(testTaille);
+console.log(testMdp);
