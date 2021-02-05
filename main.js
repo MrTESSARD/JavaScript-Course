7b981f4c079a57a48a4296d3bd4f108edb1bc151
Axel Gromat <axel.gromat@gmail.com>
2021-02-05 11:24:57 +0100

    Commit exercices

--- main.js
+++ main.js
@@ -25,7 +25,44 @@ function pairImpair(a){
 
 function vs(){
     const fighter = ["Ryu", "Ken", "Chun-Li", "Goku", "Vegeta", "Beerus", "Heihachi","Jin","Connor McGregor", "Dhalsim", "Blanka", "Khabib", "John Cena", "The Rock"];
+
+
+    function randomFighter(){
+        return fighter[Math.floor(Math.random() * fighter.length)];
+    }
+
+    let fighter1 = randomFighter();
+    let fighter2 = randomFighter();
+
+    //let fighter1 = fighter[Math.floor(Math.random() * fighter.length)];
+    //let fighter2 = fighter[Math.floor(Math.random() * fighter.length)];
+    /*
+    let fighter1 = fighter[random()];
+    let fighter2 = fighter[random()];
+    */
+    return `Get ready for the next battle ! ${fighter1} VS ${fighter2}`;
 }
+
+function SFxTekkenxDBZ(){
+    const sfPool = ["Ryu","Ken","Chun-li","Akuma","Gouken","Bison","Guile","Dhalsim","Seth","Vega","Balrog","Ibuki","Sagat","Cammy","Blanka","E.Honda","Karin","Sakura","Zangief","Rose"];
+    const dbfzPool =  ["SS Goku","SS Vegeta", "GT Goku", "C-21", "C-18", "C-17","C-16", "Super Baby 2", "Janemba", "SSGSS Gogeta", "SSGSS Goku", "Beerus", "Ginyu", "Cell", "Frieza", "Cooler", "A.Gohan", "T.Gohan", "Zamasu Fused", "Goku Black", "Jiren", "Hit", "Kid Buu", "Majin Buu", "Tien" , "Yamcha", "Nappa", "Trunks", "Bardock", "Broly", "Broly (DBS)","Videl", "Kefla", "UI Goku", "Piccolo", "SSGSS Vegito"];
+    const tekkenPool = ["Jin","Heihachi","Kazuya","Alisa","Leroy","Master Raven","Kuma","Panda","Akuma (Tekken)","Steve","Lee","Lei","Paul","Law","Nina","Yoshimitsu","Hwoarang","King","Armor King","Xiaoyu","Brian","Eddy","Anna","Asuka","Feng","Lili","Ganryu","Devil Jin","Bob","Leo","Miguel","Lars","Claudio","Katarina","Lucky Chloe","Shaheen","Josie","Gigas","Jack-7","Kazumi","Eliza","Marduk","Zafina","Kunimitsu","Negan (TWD)","Fahkumram","Geese","Noctis"];
+    const fullPool = [sfPool,dbfzPool,tekkenPool];
+    function randomTeam(){
+        const team = [];
+        for(let pool of fullPool){
+            let char = pool[Math.floor(Math.random() * pool.length)];
+            console.log(char);
+            team.push(char)
+        }
+        console.log(team);
+        return team.join("/");
+    }
+    return `Time for the big showdown ! Team 1 : ${randomTeam()} VS Team 2 : ${randomTeam()}`
+}
+
+console.log(vs());
+console.log(SFxTekkenxDBZ());
 
 
 console.log(pairImpair(2))
