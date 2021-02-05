af494cf31ee4947897a4aff23c4591dac92fa91d
Axel Gromat <axel.gromat@gmail.com>
2021-02-05 09:37:20 +0100

    Exercice vs

--- main.js
+++ main.js
@@ -1,69 +1,34 @@
-// FUNCTIONS
-/* Les fonctions et leur utilité reposent sur deux principes
-    DRY & WET :
-    Don't Repeat Yourself : Si dans mon édition de code, je me retrouve à coder plusieurs fois la même chose. Il y a de fortes chances qu'une function suffise pour accélerer mon travail.
-    Write Everything Twice, but not thrice : Vous pouvez écrire tout 2 fois, mais pas 3. C'est le principe de vous imposer des limites dans la répétition.
-*/
+// EXERCICES SUR LES FUNCTIONS
 
-// Une fonction possède généralement un nom, des arguments (ou pas), et un body.
-
-// Le nom de la fonction sert à la rappeler par la suite
-
-// Les arguments servent à renseigner les valeurs/le type de valeurs qui sont attendus dans la fonction
-
-// Le body renseigne sur les instructions à exécuter un efois la fonction appelée
-
-// Forme déclarative et expressions : Les déclarations possèdent du hoisting que les expressions n'ont pas. On utilise généralement les expressions pour des fonctions anonymes à executer immédiatement (IIFE)
-
-// FORME DECLARATIVE
-function addition(a,b){// déclaration avec le mot-clé function suivi de son nom et entre parenthèses son/ses arguments facultatifs. Ensuite vient le body de votre function qui va exécuter un ensemble d'instructions.
-    return a + b;
-}
-
-// FORME EXPRESSIVE
-const spelling = function(phrase="TALIS"){
-    return phrase.split("");
+function compteurVoyelles(string){
+    // Initialiser mon compteur
+    let compteur = 0;
+    // DECLARER LES VOYELLES
+    // let splitVoyelles = "aeiouy".split('');
+    const voyelles = ["a","e","i","o","u","y"];
+    // BOUCLE FOR POUR VERIFIER
+    lowerString = string.toLowerCase();
+    for (let carac of lowerString){
+        if(voyelles.includes(carac)){
+            //LA LETTRE EST UNE VOYELLE
+            compteur++;
+        }
+    }
+    return compteur;
 };
 
-console.log(addition(1,2));
-console.log(addition(4,5));
-
-const prixDesCourses = addition(15,5);
-console.log(addition(2,5));
-console.log(spelling());
-
-console.log(prixDesCourses)
-
-let test = 3;
-
-// Les fonctions pures sont des fonctions qui ne reposent que sur leurs propres arguments et valeurs. Qui ne produit pas d'effets secondaires.
-
-// Scope d'une fonction
-
-let global = 5;
-
-function displayGlobal(){
-
-    return global;
-}
-function displayLocal(){
-    let local = "local";
-    return local;
+function pairImpair(a){
+    return !(a%2);
 }
 
-console.log(global);//5
-console.log(displayGlobal());//5
-// console.log(local); // "local" si c'est une var, undefined autrement
-console.log(displayLocal()); // "local"
+// ENONCE : A PARTIR DE L'ARRAY SUIVANT, FAIRE UNE FONCTION QUI CREE UNE SIMULATION DE COMBAT EN CHOISISSANT ALEATOIRE DEUX COMBATTANTS ET QUI RETOURNE UN PHRASE DU TYPE A vs B
 
-// Les function recursives s'appellent elles-mêmes à l'intérieur d'elle-même
-function recursive(){
-    global++;
-    console.log(global);
-    if(global>5000){
-        return console.log("C'est fini");
-    }
-    return recursive();
+function vs(){
+    const fighter = ["Ryu", "Ken", "Chun-Li", "Goku", "Vegeta", "Beerus", "Heihachi","Jin","Connor McGregor", "Dhalsim", "Blanka", "Khabib", "John Cena", "The Rock"];
 }
 
-// recursive();
+
+console.log(pairImpair(2))
+
+console.log(compteurVoyelles("pascal"));
+console.log(compteurVoyelles("carolane"));
