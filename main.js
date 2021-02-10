2d817d96279c7c491b7ce4a72d5f57e4ca141b84
Axel Gromat <axel.gromat@gmail.com>
2021-02-10 13:23:06 +0100

    created objects

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-// Intro class
+//! Intro classes
+
+//? Déclaration d'une classe nommée User
+class User {
+    //? Le constructeur de la classe définit des paramètres initiaux name et surname
+    constructor(name,surname) {
+        //? Initialisation des name et surname en paramètre
+        this.name = name;
+        this.surname = surname;
+    }
+}
+console.log(typeof User);
+console.log(User);
+
+let vide = new User();
+let john = new User("John","Doe");
+let cena = new User("John","Cena");
+let jane = new User("Jane","Doe");
+
+console.log(vide);
+console.log(john);
+console.log(cena);
+console.log(jane);
+
